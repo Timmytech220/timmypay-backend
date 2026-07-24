27c40fd29032bda7c0edd5b0a63ada7b80a893ff
Timmytech220 <oluwatimileyindaniel4@gmail.com>
2026-07-24 18:35:21 +0100

    index.js

--- index.js
+++ index.js
@@ -20,13 +20,23 @@ app.use(express.json());
 
 // --- FIREBASE SETUP ---
 let db;
+
 try {
     if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+
+        const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+        console.log("Firebase Project ID:", serviceAccount.project_id);
+
         admin.initializeApp({
-            credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT))
+            credential: admin.credential.cert(serviceAccount)
         });
+
         db = admin.firestore();
+
         console.log("Firebase Admin Initialized ✅");
+    } else {
+        console.error("FIREBASE_SERVICE_ACCOUNT environment variable not found.");
     }
 } catch (err) {
     console.error("Firebase Init Error:", err.message);
