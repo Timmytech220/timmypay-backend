98ae38dc5b5cbaf750b0ed2d21f594de95835e5a
Timmytech220 <oluwatimileyindaniel4@gmail.com>
2026-08-04 21:57:43 +0100

    firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,1 +1,26 @@
+import admin from "firebase-admin";
 
+let db;
+
+try {
+    if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+
+        const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+        console.log("Firebase Project ID:", serviceAccount.project_id);
+
+        admin.initializeApp({
+            credential: admin.credential.cert(serviceAccount)
+        });
+
+        db = admin.firestore();
+
+        console.log("Firebase Admin Initialized ✅");
+    } else {
+        console.error("FIREBASE_SERVICE_ACCOUNT environment variable not found.");
+    }
+} catch (err) {
+    console.error("Firebase Init Error:", err.message);
+}
+
+export { admin, db };
